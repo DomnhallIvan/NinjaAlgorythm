9b21a1c0fccdc28a024d34ff0faa462c7af8b2bb
Ivan PM <cdmx2125@amerike.edu.mx>
2023-10-16 16:18:15 -0600

    SetBruh

--- Assets/Scripts/Set.cs
+++ Assets/Scripts/Set.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Set : MonoBehaviour
 {
-    public List<Vector3> set;
+    public interface IEnumerable<out T>: System.Collections.IEnumerable
+    {
+        //public List<T> set;
+    }
+    //
 
 
 }
