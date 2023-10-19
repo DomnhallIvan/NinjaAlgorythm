d9ed71ace18aa480599898eb22bba396584b9bb6
Ivan PM <cdmx2125@amerike.edu.mx>
2023-10-19 13:00:42 -0600

    MEEEEEEDICCCCCC

--- Assets/Scripts/FloodFill.cs
+++ Assets/Scripts/FloodFill.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using static UnityEditor.FilePathAttribute;
 
 public class FloodFill : MonoBehaviour
 {
     public Tilemap tM;
-    private Queue<Vector3Int> frontier = new Queue<Vector3Int>();
+    [SerializeField] private Queue<Vector3Int> frontier = new Queue<Vector3Int>();
     public Vector3Int startingPoint;
     public Set reached = new Set();
     public Tile tile1;
@@ -18,11 +19,13 @@ public class FloodFill : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             CumJar();
+            Debug.Log("IMBOUTTOCUM");
         }
        
     }
     void CumJar()
     {
+        frontier.Enqueue(startingPoint);
         //Funcion que devuelva lista de Vector3.int, a partir de current
         while (frontier.Count > 0)
         {
@@ -31,7 +34,7 @@ public class FloodFill : MonoBehaviour
             {
                 if (!reached.set.Contains(neighbors))
                 {
-                    if (neighbors != null)
+                    if (neighbors != null&& tM.GetSprite(neighbors) != null)
                     {
                         AddReached(neighbors);
                         frontier.Enqueue(neighbors);
@@ -63,18 +66,8 @@ public class FloodFill : MonoBehaviour
 
     //Aquí tengo que añadir los vecinos que alcance para no tomarlos en cuenta en siguientes pruebas
     public void AddReached(Vector3Int Reached)
-    {
-        Reached = startingPoint;   
+    {    
         reached.set.Add(Reached);
     }
-    
-   
-    /*
-    public Vector3Int getNeighbourZ(Vector3Int current)
-    {
-        List<Vector3Int> neighbours = getNeighbourZ(current);
-        return current;
-    }*/
-
 
 }
